46c5c554fc0d272aac1c9e2c3b33a552d30eafa6
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-02-27 01:42:16 +0700

    Update package

--- main.go
+++ main.go
@@ -2,33 +2,12 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/sumitroajiprabowo/learn-gin-gonic/v1/example"
+	"github.com/sumitroajiprabowo/learn-gin-gonic/handler"
 )
-
-// SetupRouter is a function to setup router
-func SetupRouter() *gin.Engine {
-	r := gin.Default() // Default gin router
-
-	r.LoadHTMLGlob("templates/*") // Load html templates
-
-	r.GET("/api/basic/example", example.BasicExample)
-	r.GET("/api/basic/example/struct", example.BasicExampleWithStruct)
-	r.GET("/api/basic/example/map", example.BasicExampleWithMap)
-	r.GET("/api/basic/example/ascii", example.BasicExampleAsciiJson)
-	r.GET("/api/basic/bindrequest/teacher", example.GetBindRequestNestedStruct)
-	r.GET("/api/basic/bindrequest/student", example.GetBindRequestNestedStructPointer)
-	r.GET("/api/basic/bindrequest/employee", example.GetBindRequestNestedAnonyStruct)
-	r.GET("/api/basic/bindrequestformdata", example.IndexFormHandler)
-	r.POST("/api/basic/bindrequestformdata", example.FormHandler)
-	r.GET("/api/bindquerystring", example.BindQueryString)
-	r.GET("/api/binduri/:name/:age", example.BindURI)
-
-	return r
-}
 
 // RunServer is a function to run server
 func main() {
 	gin.SetMode(gin.ReleaseMode)
-	router := SetupRouter() // Setup router
-	router.Run(":3000")     // Run on port 3000
+	router := handler.SetupRouter() // Setup router
+	router.Run(":3000")             // Run on port 3000
 }
